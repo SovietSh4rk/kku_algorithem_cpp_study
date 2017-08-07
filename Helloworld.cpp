19d13f1fa066123c809c7d1c225ac750f5570c22
SovietSh4rk <temeralre@naver.com>
2017-08-07 20:41:08 +0900

    Helloworld.cpp
    0807

---
+++ Helloworld.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+Int main(void)
+
+{
+ int num = 20;
+ std::cout << “Hello world!” << std::endl;
+ std::cout << “Hello “ << “world” << std::endl;
+ std::cout << num << ‘ ‘ <<‘A’;
+ std::cout << ‘    ‘ <<3.14<<std::endl;
+
+ return 0;
+}
