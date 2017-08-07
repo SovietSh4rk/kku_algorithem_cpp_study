c7a5bb4f10f2a2a0bf4976d560a87e811f35b6f2
SovietKozix <temeralre@naver.com>
2017-08-07 22:33:38 +0900

    Update Simple Adder.cpp

--- Simple Adder.cpp
+++ Simple Adder.cpp
@@ -2,7 +2,7 @@
 {
 	int val1;
 	std::cout<<"첫 번째 숫자를 입력하세요:";
-  std:cin>>val1;
+ 	std:cin>>val1;
 
   int val2;
 	std::cout<"두 번째 숫자를 입력하세요:";
@@ -10,5 +10,6 @@
 
 	int result=val1+val2;
 	std::cout<<"덧셈결과 : " < result << std::endl;
+	
    return 0;
 }
